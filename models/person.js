96aa757058d29a54bdffbc2bae9080a2d6832d80
MattiKaleva <matti.kaleva1@gmail.com>
2023-12-11 18:18:00 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -17,8 +17,23 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    message: 'Name must be at least 3 characters long'
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{7,8}/.test(v)
+      },
+      message: 'Invalid number, must be xx-xxxxxxxx or xxx-xxxxxxx'
+    },
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
